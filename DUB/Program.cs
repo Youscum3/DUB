e3b7dc3657603a40baebd782d9c973e26cd3967d
Youscum3 <khkhkh.20@list.ru>
2026-02-12 23:36:59 +0200

    Add Dockerfile

--- DUB/Program.cs
+++ DUB/Program.cs
@@ -1,9 +1,6 @@
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddControllers();
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
@@ -11,16 +8,7 @@ var app = builder.Build();
 var port = Environment.GetEnvironmentVariable("PORT") ?? "8080";
 app.Urls.Add($"http://*:{port}");
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
-app.UseAuthorization();
-
+app.UseRouting();
 app.MapControllers();
 
 app.Run();
